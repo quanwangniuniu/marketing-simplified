a93cb8ec9acbf69ae1403c295ab252db830f4848
yulinliu <liuyulin7@outlook.com>
2026-08-22 11:04:07 +1000

    fix(audit): fix __init__.py empty bug

--- backend/audit/apps.py
+++ backend/audit/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class AuditConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'audit'
+
+    def ready(self):
+        pass  # Import audit signals here when added, e.g.: import audit.signals
